3d64c9a4e0d6164e3e1e37c94428e7c5410fd9f5
Sky <skylarplayz348@gmail.com>
2025-06-28 05:02:57 -0500

    Update 6/28/25

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ int main(int argc, char const *argv[])
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
     SDL_Texture *texture = NULL;
+    SDL_Texture *texture2 = NULL;
     SDL_FRect rect;
     rect.x = 0;
     rect.y = 0;
@@ -16,8 +17,10 @@ int main(int argc, char const *argv[])
     rect.h = height;
     bool quit = false;
     bool team_red = false;
+    bool key_pressed = false;
 
     SDL_SetAppMetadata("Sky Vincent SDL Game", "1.0", "com.skyvincent.sdl-game");
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "SDL Game", "Starting", NULL);
 
     if (!SDL_Init(SDL_INIT_VIDEO))
     {
@@ -26,10 +29,11 @@ int main(int argc, char const *argv[])
 
     if (!SDL_CreateWindowAndRenderer("SDL Game", width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_TRANSPARENT, &window, &renderer))
     {
-        SDL_Log("Failed to create window and/or renderer", SDL_GetError());
+        SDL_Log("Failed to create window and/or renderer %s", SDL_GetError());
     }
-
+    SDL_GetBasePath();
     texture = IMG_LoadTexture(renderer, "../resources/blue.png");
+    texture2 = IMG_LoadTexture(renderer, "../resources/red.png");
     if (!texture)
     {
         SDL_Log("No Image: %s", SDL_GetError());
@@ -45,13 +49,34 @@ int main(int argc, char const *argv[])
             case SDL_EVENT_QUIT:
                 quit = true;
                 break;
+            case SDL_EVENT_KEY_DOWN:
+                switch (event.key.key)
+                {
+                case SDLK_ESCAPE:
+                    quit = true;
+                    break;
+
+                default:
+                    key_pressed = true;
+                    break;
+                }
+                break;
+            case SDL_EVENT_KEY_UP:
+                key_pressed = false;
+                break;
             }
         }
-
-        SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColorFloat(renderer, 0.0f, 0.0f, 0.0f, 0.0f);
+        if (key_pressed)
+            team_red = true;
+        else
+            team_red = false;
         SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
-        SDL_RenderTexture(renderer, texture, NULL, &rect);
+        SDL_SetRenderDrawColorFloat(renderer, 0.0f, 0.0f, 0.0f, 0.0f);
+        SDL_RenderClear(renderer);
+        if (!team_red)
+            SDL_RenderTexture(renderer, texture, NULL, &rect);
+        else
+            SDL_RenderTexture(renderer, texture2, NULL, &rect);
         SDL_RenderPresent(renderer);
     }
 
